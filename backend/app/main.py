1384b5c7079bd3595e0d0426cbc30aa0304b4aa8
Aneesha <aneeshadsouza2002@gmail.com>
2026-02-25 18:41:24 +0530

    allow origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:3000", "http://localhost:3000"],
+    allow_origins=["https://seat-booking-ui.vercel.app"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
