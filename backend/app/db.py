1ca65cd5b07f1f2bdccd9d1ee1b98ed2cdb44e11
Aneesha D Souza <aneeshadsouza2002@gmail.com>
2026-04-12 16:35:28 +0530

    Changed to postgresql

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 #Database connection
-DATABASE_URL = "sqlite:///./seats.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"} if DATABASE_URL.startswith("postgresql") else {},
+    pool_pre_ping=True,
+    pool_recycle=300
+)
 
 #Session factory
 SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
