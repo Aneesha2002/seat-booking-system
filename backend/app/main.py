af4d9a027a5274efb9a41be3aef731458bff6842
Aneesha D Souza <aneeshadsouza2002@gmail.com>
2026-04-12 18:36:49 +0530

    CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,10 +19,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://seat-booking-system-omega.vercel.app"
-    ],
+    #allow_origins=["http://localhost:3000","https://seat-booking-system-omega.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
