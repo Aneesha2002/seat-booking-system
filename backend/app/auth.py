dc0f4550a1a090a814d0feb455d83b77e5e952a4
Aneesha D Souza <aneeshadsouza2002@gmail.com>
2026-04-12 19:14:36 +0530

    Moved authentication logic

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -18,7 +18,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
 
 def create_access_token(data: dict, expires_delta: timedelta | None= None):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + (
+    expire = datetime.utcnow() + (
         expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     to_encode.update({"exp": expire})
